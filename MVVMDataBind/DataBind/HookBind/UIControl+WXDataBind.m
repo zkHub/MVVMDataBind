c01e04cdc678aab51307a862c777517ff515579f
zhangke14 <zhangke14@100tal.com>
2021-09-26 18:49:42 +0800

    1

--- MVVMDataBind/DataBind/HookBind/UIControl+WXDataBind.m
+++ MVVMDataBind/DataBind/HookBind/UIControl+WXDataBind.m
@@ -9,32 +9,31 @@
 #import <objc/runtime.h>
 
 @interface UIControl ()
-@property(nonatomic, copy) NSString *db_ctrl_bindKeyPath;
+@property(nonatomic, copy) NSString *db_ctrlBindKeyPath;
 
 @end
 
 
 @implementation UIControl (WXDataBind)
 
-- (NSString *)db_ctrl_bindKeyPath {
-    NSString *targetKeyHash = objc_getAssociatedObject(self, _cmd);
-    return targetKeyHash?:@"";
+- (NSString *)db_ctrlBindKeyPath {
+    return objc_getAssociatedObject(self, @selector(db_ctrlBindKeyPath));
 }
 
-- (void)setDb_ctrl_bindKeyPath:(NSString *)db_ctrl_bindKeyPath {
-    objc_setAssociatedObject(self, @selector(db_ctrl_bindKeyPath), db_ctrl_bindKeyPath, OBJC_ASSOCIATION_COPY_NONATOMIC);
+- (void)setdb_ctrlBindKeyPath:(NSString *)db_ctrlBindKeyPath {
+    objc_setAssociatedObject(self, @selector(db_ctrlBindKeyPath), db_ctrlBindKeyPath, OBJC_ASSOCIATION_COPY_NONATOMIC);
 }
 
 - (WXDBWatcher *)db_addBindUIObserverWithKeyPath:(NSString *)keyPath forControlEvents:(UIControlEvents)controlEvent convertBlock:(WXDBAnyBlock)convertBlock {
     [self addTarget:self action:@selector(valueChange:) forControlEvents:(controlEvent)];
     WXDBWatcher *watcher = [[WXDBWatcher alloc] initWithTarget:self keyPath:keyPath convertBlock:convertBlock];
     [self db_setWatcher:watcher forKey:[self db_watcherKeyWithKeyPath:keyPath]];
-    self.db_ctrl_bindKeyPath = keyPath;
+    self.db_ctrlBindKeyPath = keyPath;
     return watcher;
 }
 
 - (void)valueChange:(UIControl *)target {
-    WXDBWatcher *watcher = [self db_watcherForKey:[self db_watcherKeyWithKeyPath:self.db_ctrl_bindKeyPath]];
+    WXDBWatcher *watcher = [self db_watcherForKey:[self db_watcherKeyWithKeyPath:self.db_ctrlBindKeyPath]];
     if (watcher) {
         [watcher notify];
     }
